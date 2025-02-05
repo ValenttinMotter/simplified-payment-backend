32a4644cca9a2724630c3f880ef51074fa5250a0
Valenttin Motter <valenttin@myscrumhalf.com>
2025-02-04 21:33:47 -0300

    init files

---
+++ src/main/java/dev/valenttin/desafio_backend_pagamento/DesafioBackendPagamentoApplication.java
@@ -0,0 +1,13 @@
+package dev.valenttin.desafio_backend_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendPagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/valenttin/desafio_backend_pagamento/DesafioBackendPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.valenttin.desafio_backend_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
